def55610562558eebc97252cbe9b83789ccaa18d
Adam Paszke <adam.paszke@gmail.com>
2016-03-02 21:56:26 +0100

    Improve error handling
    When an error occurs in some module, all containers up to the
    topmost one will be printed now.
    
    Also, removed zeroGradParameters from ConcatTable, because it was
    no different from its parent's implementation.

--- Container.lua
+++ Container.lua
@@ -20,6 +20,48 @@ function Container:size()
     return #self.modules
 end
 
+-- module argument can be retrieved with moduleIndex, but code is cleaner when
+-- it has to be specified anyway.
+function Container:rethrowErrors(module, moduleIndex, funcName, ...)
+    local ERROR_HEADER = "======== ERROR ========"
+    local TRACEBACK_WARNING = "WARNING: If you see a stack trace below, it doesn't point to the place where this error occured. Please use only the one above."
+    local function handleError(err)
+       local err_lines = err:split('\n')
+       -- This should be executed only in the first container that handles the
+       -- error. First line will be appended by lua when calling error, then
+       -- there should be our header.
+       if #err_lines < 2 or err_lines[2] ~= ERROR_HEADER then
+          local traceback = debug.traceback()
+          -- Remove this handler from the stack
+          local _, first_line_len = traceback:find('^.-\n')
+          local _, second_line_end = traceback:find('^.-\n.-\n')
+          traceback = traceback:sub(1, first_line_len) .. traceback:sub(second_line_end+1)
+          err = err .. '\n' .. traceback .. '\n\n' .. TRACEBACK_WARNING
+       else
+          -- +2 for newlines
+          local second_line_end = #err_lines[1] + #err_lines[2] + 2
+          err = err:sub(second_line_end+1)
+       end
+       local msg = string.format('In %d module of %s',
+                                 moduleIndex, torch.type(self))
+       -- Preceding newline has to be here, because lua will prepend a file path.
+       err = '\n' .. ERROR_HEADER .. '\n' .. msg .. '\n' .. err
+       return err
+    end
+
+    assert(module == self.modules[moduleIndex], "mismatch between moduleIndex and self.modules in rethrowErrors")
+    local args = {...}
+    local ok, ret, noret = xpcall(function()
+                                     return module[funcName](module, unpack(args))
+                                  end,
+                                  handleError)
+    assert(noret == nil, "rethrowErrors supports only one return argument")
+    if not ok then
+       error(ret)
+    end
+    return ret
+end
+
 function Container:applyToModules(func)
     for _, module in ipairs(self.modules) do
         func(module)
